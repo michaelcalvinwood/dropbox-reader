206aa3559e1d7c5d486fb7894fc79bbde1328ce2
Michael Wood <michaelwood33311@icloud.com>
2023-09-18 12:36:32 -0400

    added recursively getting all file names

--- dropbox-reader.js
+++ dropbox-reader.js
@@ -9,7 +9,51 @@ const axios = require('axios');
 
 const { DROPBOX_TOKEN } = process.env;
 
-const dropboxTest = async () => {
+const getFileUrl = async (id) => {
+    const request = {
+        url: 'https://api.dropboxapi.com/2/file_requests/get',
+        method: 'post',
+        headers: {
+            "Authorization": `Bearer ${DROPBOX_TOKEN}`,
+            "Content-Type": "application/json"
+        },
+        data: {
+           id
+        }
+    }
+
+    try {
+        response = await axios(request);
+        return(response.data);
+    } catch(err) {
+        console.error(err);
+        return false;
+    }
+}
+
+const listContinue = async (cursor) => {
+    const request = {
+        url: 'https://api.dropboxapi.com/2/files/list_folder/continue',
+        method: 'post',
+        headers: {
+            "Authorization": `Bearer ${DROPBOX_TOKEN}`,
+            "Content-Type": "application/json"
+        },
+        data: {
+            cursor
+        }
+    }
+
+    try {
+        response = await axios(request);
+        return response.data
+    } catch(err) {
+        console.error(err);
+        return false;
+    }
+}
+
+const listFiles = async (path) => {
     const request = {
         url: 'https://api.dropboxapi.com/2/files/list_folder',
         method: 'post',
@@ -23,16 +67,31 @@ const dropboxTest = async () => {
             include_media_info: false,
             include_mounted_folders: true,
             include_non_downloadable_files: true,
-            recursive: false,
-            path: "/DataVizFolder/connectedEconomy"
+            recursive: true,
+            path
         }
     }
 
     try {
         response = await axios(request);
-        console.log(response.data);
+        return response.data
     } catch(err) {
         console.error(err);
+        return false;
     }
 }
 
+const getList = async () => {
+    const list = [];
+
+    let files = await listFiles('/DataVizFolder/');
+    let { has_more } = files;
+    let { cursor } = files;
+    while (has_more) {
+        let files = await listContinue(cursor);
+        has_more = files.has_more;
+        cursor = files.cursor
+    }
+}
+
+getList();
